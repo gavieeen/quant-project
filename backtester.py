4beec27a71472c5244f8c596371e4c68dbb251da
gavieeen <gavinebenezer@gmail.com>
2024-07-02 21:29:11 -0500

    financial api test

--- backtester.py
+++ backtester.py
@@ -1,8 +1,11 @@
+import pandas as pd
+import numpy as np
 
 #based on the code given itll take the data from some api and return it as a dataframe, the split function will give the df split for testing 
 #and training. Eg if you put 70 in split, it will give you a dataframe that's 70 percent the og and the other 30 will be for backtest.
 #n will just be the size of the df.
 def getstockdata(stockcode,split,n):
+    stock_data = pd.read_csv(stockcode)
     pass
 
 
